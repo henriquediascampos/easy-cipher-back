4efd9b0be253e4e677162d19fffb2f18bf5a4e27
Henrique Dias Campos <henrique.campos@mv.com.br>
2021-10-12 01:04:28 -0300

    add policy cors

--- easy-cipher-controller/src/main/java/com/easycipherback/webConfig/webConfig.java
+++ easy-cipher-controller/src/main/java/com/easycipherback/webConfig/webConfig.java
@@ -1,8 +1,10 @@
 
 package com.easycipherback.webConfig;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
@@ -11,6 +13,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableWebMvc
 @Configuration
 public class webConfig implements WebMvcConfigurer {
+
+    @Value("${origin-client}")
+    private String origin;
+
     @Override
     public void configurePathMatch(final PathMatchConfigurer configurer) {
         configurer.addPathPrefix("api/", c ->  c.isAnnotationPresent(RestController.class) && !c.getName().contains("swagger"));
@@ -20,4 +26,13 @@ public class webConfig implements WebMvcConfigurer {
     public void addViewControllers(ViewControllerRegistry registry) {
         // registry.addViewController("/").setViewName("index");
     }
+
+    public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/greeting-javaconfig").allowedOrigins(origin);
+			}
+		};
+	}
 }
